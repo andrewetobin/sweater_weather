e6f3a224ded4ac3976b2e188395df648e193b548
andrewetobin <andrewetobin@gmail.com>
2018-11-05 19:16:37 -0700

    selecting about 40 lines to use out of a 1600 line api response

--- app/serializers/forecast_finder_serializer.rb
+++ app/serializers/forecast_finder_serializer.rb
@@ -1,5 +1,5 @@
 class ForecastFinderSerializer < ActiveModel::Serializer
-  attributes :location, :currently, :details
+  attributes :location, :currently, :details, :forecast
 
   def location
     object.formatted_address
@@ -10,6 +10,7 @@ class ForecastFinderSerializer < ActiveModel::Serializer
       "current_time" => Time.at(object.info[:currently][:time]),
       "current_temperature" => object.info[:currently][:temperature].round,
       "current_summary" => object.info[:currently][:summary],
+      "icon" => object.info[:currently][:icon],
       "high" => object.info[:daily][:data][0][:temperatureHigh].round,
       "low" => object.info[:daily][:data][0][:temperatureLow].round
     }
@@ -19,6 +20,7 @@ class ForecastFinderSerializer < ActiveModel::Serializer
   def details
     [{
       "current_summary" => object.info[:currently][:summary],
+      "icon" => object.info[:currently][:icon],
       "today" => object.info[:daily][:data][0][:summary],
       "tonight" => object.info[:daily][:data][1][:summary],
       "feels_like" => object.info[:currently][:apparentTemperature].round,
@@ -28,6 +30,43 @@ class ForecastFinderSerializer < ActiveModel::Serializer
     }
     ]
   end
+
+  def forecast
+    [
+      {"day" => Time.at(object.info[:daily][:data][1][:time]),
+      "icon" => object.info[:daily][:data][1][:icon],
+      "high" => object.info[:daily][:data][1][:temperatureHigh].round,
+      "low" => object.info[:daily][:data][1][:temperatureLow].round,
+      "precip" => ((object.info[:daily][:data][1][:precipProbability])*100).to_s << " %",
+    },
+      {"day" => Time.at(object.info[:daily][:data][2][:time]),
+      "icon" => object.info[:daily][:data][2][:icon],
+      "high" => object.info[:daily][:data][2][:temperatureHigh].round,
+      "low" => object.info[:daily][:data][2][:temperatureLow].round,
+      "precip" => ((object.info[:daily][:data][2][:precipProbability])*100).to_s << " %",
+    },
+      {"day" => Time.at(object.info[:daily][:data][3][:time]),
+      "icon" => object.info[:daily][:data][3][:icon],
+      "high" => object.info[:daily][:data][3][:temperatureHigh].round,
+      "low" => object.info[:daily][:data][3][:temperatureLow].round,
+      "precip" => ((object.info[:daily][:data][3][:precipProbability])*100).to_s << " %",
+    },
+      {"day" => Time.at(object.info[:daily][:data][4][:time]),
+      "icon" => object.info[:daily][:data][4][:icon],
+      "high" => object.info[:daily][:data][4][:temperatureHigh].round,
+      "low" => object.info[:daily][:data][4][:temperatureLow].round,
+      "precip" => ((object.info[:daily][:data][4][:precipProbability])*100).to_s << " %",
+    },
+      {"day" => Time.at(object.info[:daily][:data][5][:time]),
+      "icon" => object.info[:daily][:data][5][:icon],
+      "high" => object.info[:daily][:data][5][:temperatureHigh].round,
+      "low" => object.info[:daily][:data][5][:temperatureLow].round,
+      "precip" => ((object.info[:daily][:data][5][:precipProbability])*100).to_s << " %",
+    }
+    ]
+  end
+
+
 
 
 end
--- spec/requests/user_can_see_weather_info_spec.rb
+++ spec/requests/user_can_see_weather_info_spec.rb
@@ -11,22 +11,50 @@ describe "GET '/api/v1/forecast?location=denver,co'" do
 
     forecast = JSON.parse(response.body, symbolize_names: true)
 
-    return_json =  {:location=>"Denver, CO, USA",
- :currently=>
-  [{:current_time=>"2018-11-05T10:30:54.000-07:00",
-    :current_temperature=>51,
-    :current_summary=>"Partly Cloudy",
-    :high=>53,
-    :low=>32}],
- :details=>
-  [{:current_summary=>"Partly Cloudy",
-    :today=>"Partly cloudy in the morning.",
-    :tonight=>"Partly cloudy overnight.",
-    :feels_like=>51,
-    :humidity=>"27.0 %",
-    :visibility=>"10 mile(s)",
-    :"UV Index"=>3}]
-  }
+    return_json =
+    {:location=>"Denver, CO, USA",
+    :currently=>
+     [{:current_time=>"2018-11-05T10:30:54.000-07:00",
+       :current_temperature=>51,
+       :current_summary=>"Partly Cloudy",
+       :icon=>"partly-cloudy-day",
+       :high=>53,
+       :low=>32}],
+    :details=>
+     [{:current_summary=>"Partly Cloudy",
+       :icon=>"partly-cloudy-day",
+       :today=>"Partly cloudy in the morning.",
+       :tonight=>"Partly cloudy overnight.",
+       :feels_like=>51,
+       :humidity=>"27.0 %",
+       :visibility=>"10 mile(s)",
+       :"UV Index"=>3}],
+    :forecast=>
+     [{:day=>"2018-11-06T00:00:00.000-07:00",
+       :icon=>"partly-cloudy-night",
+       :high=>49,
+       :low=>25,
+       :precip=>"8.0 %"},
+      {:day=>"2018-11-07T00:00:00.000-07:00",
+       :icon=>"clear-day",
+       :high=>44,
+       :low=>25,
+       :precip=>"9.0 %"},
+      {:day=>"2018-11-08T00:00:00.000-07:00",
+       :icon=>"clear-day",
+       :high=>43,
+       :low=>21,
+       :precip=>"0 %"},
+      {:day=>"2018-11-09T00:00:00.000-07:00",
+       :icon=>"partly-cloudy-night",
+       :high=>52,
+       :low=>28,
+       :precip=>"2.0 %"},
+      {:day=>"2018-11-10T00:00:00.000-07:00",
+       :icon=>"partly-cloudy-night",
+       :high=>59,
+       :low=>26,
+       :precip=>"1.0 %"}]}
 
     expect(forecast).to eq(return_json)
   end
