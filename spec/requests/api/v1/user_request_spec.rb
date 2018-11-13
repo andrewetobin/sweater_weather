87fbfd54396e036b3c13063d308fa0d91364ceaf
andrewetobin <andrewetobin@gmail.com>
2018-11-13 09:49:25 -0700

    adds bcrypt

---
+++ spec/requests/api/v1/user_request_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+describe "Users API" do
+  scenario 'user can post to create account' do
+    user_data = {
+      email: 'jeffcoffin@email.com',
+      password: 'password',
+      password_confirmation: 'password'
+      }
+
+      post "/api/v1/users", params: user_data
+
+      user_response = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response.status).to eq(200)
+      expect(user_response).to have_key(:api_key)
+      expect(User.last.email).to eq('kat@email.com')
+  end
+
+  xscenario 'error if email already exists' do
+    user = create(:user)
+    user_data = {
+      email: user.email,
+      password: 'password',
+      password_confirmation: 'password'
+    }
+
+    post '/api/v1/users', params: user_data
+
+    error = JSON.parse(response.body)
+
+    expect(response.status).to eq(401)
+    expect(error["message"]).to eq("Error")
+  end
+end
